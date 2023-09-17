2836f36b7d722ad148e56476d4987da19252686f
AlexanderDev2004 <alexgaming2120@gmail.com>
2023-09-17 21:57:55 +0700

    Update Java Array (pilihan) -Alex

--- Kasir_RS.java
+++ Kasir_RS.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList; // Menambahkan List (Alex)
 public class Kasir_RS{
+    
     public static void main(String[] args){
         boolean berhasil;
         berhasil = false;
@@ -16,7 +18,10 @@ public class Kasir_RS{
         String username= input.nextLine();
         System.out.print("Masukan Password: ");
         String password= input.nextLine();
-
+        ArrayList<String> daftarObat = new ArrayList<>(); //masukan Vari array (Alex)
+        ArrayList<Integer> hargaObat = new ArrayList<>(); //masukan vari array (Alex)
+        ArrayList<Integer> jumlahPembelian = new ArrayList<>();//masukan vari array (Alex) 
+        
         //Deklarasi Password
         if ((username.equals("alek")) && (password.equals("admin"))){
         berhasil = true;
@@ -34,6 +39,51 @@ public class Kasir_RS{
             System.out.println("|              Masukan yang Benar             |");
             System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");       
         }
+        // tampilan Pilihan (Alex) 
+         while (true) {
+              System.out.println("\nMenu:");
+            System.out.println("1. Tambahkan Obat");
+            System.out.println("2. Cetak Pembelian Obat");
+            System.out.println("3. Keluar");
+            System.out.print("Pilihan Anda: ");
+            
+            int pilihan = input.nextInt();
+            // memilih pilihan kita 
+            switch (pilihan) {
+                case 1:
+                    input.nextLine(); // Membuang karakter newline
+                    System.out.print("Masukkan nama obat: ");
+                    String namaObat = input.nextLine();
+                    System.out.print("Masukkan harga obat: ");
+                    int harga = input.nextInt();
+                    System.out.print("Masukkan jumlah obat yang dibeli: ");
+                    int jumlah = input.nextInt();
+
+                    daftarObat.add(namaObat);
+                    hargaObat.add(harga);
+                    jumlahPembelian.add(jumlah);
+
+                    System.out.println("Obat berhasil ditambahkan!");
+                    break;
+                case 2:
+                    System.out.println("Daftar Obat yang Dibeli:");
+
+                    int totalHarga = 0;
+                    for (int i = 0; i < daftarObat.size(); i++) {
+                        int subtotal = hargaObat.get(i) * jumlahPembelian.get(i);
+                        System.out.println(daftarObat.get(i) + " - Harga: " + hargaObat.get(i) + " - Jumlah: " + jumlahPembelian.get(i) + " - Subtotal: " + subtotal);
+                        totalHarga += subtotal;
+                    }
+                    System.out.println("Total Harga: " + totalHarga);
+                    break;
+                case 3:
+                    System.out.println("Terima kasih telah menggunakan program ini!");
+                    System.exit(0);
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        }
+         
     }
     }
 }
