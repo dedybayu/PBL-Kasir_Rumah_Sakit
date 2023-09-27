c3bfe88759834e03c658dda5cdabca58e36be969
dedybayusetiawan <dedybayusetiawan5@gmail.com>
2023-09-27 18:43:39 +0700

    update pemilihan

--- Kasir_RS.java
+++ Kasir_RS.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-import java.util.ArrayList; // Menambahkan List (Alex)
 public class Kasir_RS{
-    
     public static void main(String[] args){
         boolean berhasil;
         berhasil = false;
@@ -18,72 +16,75 @@ public class Kasir_RS{
         String username= input.nextLine();
         System.out.print("Masukan Password: ");
         String password= input.nextLine();
-        ArrayList<String> daftarObat = new ArrayList<>(); //masukan Vari array (Alex)
-        ArrayList<Integer> hargaObat = new ArrayList<>(); //masukan vari array (Alex)
-        ArrayList<Integer> jumlahPembelian = new ArrayList<>();//masukan vari array (Alex) 
-        
+     
         //Deklarasi Password
-        if ((username.equals("alek")) && (password.equals("admin"))){
+        if ((username.equals("rscintajava")) && (password.equals("javasejati"))){
         berhasil = true;
-
+            String kamar;
             System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");
             System.out.println("|           Selamat Datang di Kasir           |");
             System.out.println("|           Rumah Sakit Cina Sejati           |");
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");    
+            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");  
+            System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++");
+            System.out.println("|          Silahkan Pilih Kelas Kamar         |");
+            System.out.println("|             1. VIP                          |");
+            System.out.println("|             2. Reguler                      |");
+            System.out.println("|             3. BPJS                         |");
+            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");
+            System.out.print("Masukan  Pilihan: ");
+            kamar = input.nextLine();
+
+            int lamaInapVIP, lamaInapReg, lamaInapBPJS, obat, konsumsi, total; 
+
+            if (kamar.equals ("1")){
+                System.out.print("Lama Menginap (Hari)     : ");
+                lamaInapVIP = input.nextInt();
+                System.out.print("Layanan Obat per Perhari : ");
+                obat = input.nextInt();
+                System.out.print("Layanan Konsumsi Perhari : ");
+                konsumsi = input.nextInt();
+                
+                total = (1000000*lamaInapVIP) + (obat*100000*lamaInapVIP) + (konsumsi*50000*lamaInapVIP);
+
+                System.out.println("Total Tagihan            : " + total);
+            }
+            else if (kamar.equals ("2")){
+                System.out.print("Lama Menginap (Hari)     : ");
+                lamaInapReg = input.nextInt();
+                System.out.print("Layanan Obat per Perhari : ");
+                obat = input.nextInt();
+                System.out.print("Layanan Konsumsi Perhari : ");
+                konsumsi = input.nextInt();
+                
+                total = (700000*lamaInapReg) + (obat*100000*lamaInapReg) + (konsumsi*50000*lamaInapReg);
+
+                System.out.println("Total Tagihan            : " + total);
+            }
+            else if (kamar.equals ("3")){
+                System.out.print("Lama Menginap (Hari)     : ");
+                lamaInapBPJS = input.nextInt();
+                System.out.print("Layanan Obat per Perhari : ");
+                obat = input.nextInt();
+                System.out.print("Layanan Konsumsi Perhari : ");
+                konsumsi = input.nextInt();
+                
+                total = (500000*lamaInapBPJS) + (obat*100000*lamaInapBPJS) + (konsumsi*50000*lamaInapBPJS);
+
+                System.out.println("Total Tagihan            : " + total);            
+            }
+            else{
+                System.out.println("eror");
+            }
+
         }
 
         else{
             System.out.println(" ");
             System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");
-            System.out.println("|               Passwordmu Salah              |");
+            System.out.println("|        Username Atau Passwordmu Salah       |");
             System.out.println("|              Masukan yang Benar             |");
             System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");       
         }
-        // tampilan Pilihan (Alex) 
-         while (true) {
-              System.out.println("\nMenu:");
-            System.out.println("1. Tambahkan Obat");
-            System.out.println("2. Cetak Pembelian Obat");
-            System.out.println("3. Keluar");
-            System.out.print("Pilihan Anda: ");
-            
-            int pilihan = input.nextInt();
-            // memilih pilihan kita 
-            switch (pilihan) {
-                case 1:
-                    input.nextLine(); // Membuang karakter newline
-                    System.out.print("Masukkan nama obat: ");
-                    String namaObat = input.nextLine();
-                    System.out.print("Masukkan harga obat: ");
-                    int harga = input.nextInt();
-                    System.out.print("Masukkan jumlah obat yang dibeli: ");
-                    int jumlah = input.nextInt();
-
-                    daftarObat.add(namaObat);
-                    hargaObat.add(harga);
-                    jumlahPembelian.add(jumlah);
-
-                    System.out.println("Obat berhasil ditambahkan!");
-                    break;
-                case 2:
-                    System.out.println("Daftar Obat yang Dibeli:");
-
-                    int totalHarga = 0;
-                    for (int i = 0; i < daftarObat.size(); i++) {
-                        int subtotal = hargaObat.get(i) * jumlahPembelian.get(i);
-                        System.out.println(daftarObat.get(i) + " - Harga: " + hargaObat.get(i) + " - Jumlah: " + jumlahPembelian.get(i) + " - Subtotal: " + subtotal);
-                        totalHarga += subtotal;
-                    }
-                    System.out.println("Total Harga: " + totalHarga);
-                    break;
-                case 3:
-                    System.out.println("Terima kasih telah menggunakan program ini!");
-                    System.exit(0);
-                default:
-                    System.out.println("Pilihan tidak valid.");
-            }
-        }
-         
     }
     }
 }
