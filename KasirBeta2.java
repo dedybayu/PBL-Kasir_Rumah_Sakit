8a35177861d110d1daeab09aea8e87594c5a85ec
AlexanderDev2004 <alexgaming2120@gmail.com>
2023-11-16 12:34:53 +0700

    fiks bug

--- KasirBeta2.java
+++ KasirBeta2.java
@@ -527,6 +527,10 @@ public class KasirBeta2 {
                                                         }
                                                         System.out.println(
                                                                 "Pasien didaftarkan di KamarBersama " + (i + 1));
+
+                                                        // for (int k = 0; k < kamarBersama[i][j].length; k++) {
+                                                        //     System.out.println(kamarBersama[i][j][k]);
+                                                        // }
                                                         break;
                                                     }
                                                 }
@@ -652,8 +656,8 @@ public class KasirBeta2 {
                                 break;
 
                             case 3:
-                                for (int i = 0; i > kamarBersama.length; i++) {
-                                    for (int j = 0; j > kamarBersama[i].length; j++) {
+                                for (int i = 0; i < kamarBersama.length; i++) {
+                                    for (int j = 0; j < kamarBersama[i].length; j++) {
                                         if (kamarBersama[i][j][0] != null) {
                                             System.out.println("Nama Pasien : " + kamarBersama[i][j][0]);
                                             adaYangInap = true;
