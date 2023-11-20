82893a3dadcc5038da8b2e718b7b28c02ffc8bd7
AlexanderDev2004 <alexgaming2120@gmail.com>
2023-11-20 21:16:59 +0700

    update ngicil

--- KasirBeta2.java
+++ KasirBeta2.java
@@ -807,10 +807,20 @@ public class KasirBeta2 {
             switch (menuManager) {
                 case 1:
                     // Menu Laporan Keuangan
-                    System.out.println();
                     System.out.println("Menu Laporan Keuangan");
-                    System.out.println("Masukan kode pasien :");
-                    input.kodePasien
+                    System.out.println("========================================");
+                    System.out.println("Masukan kode pasien : ");
+                    System.out.println("========================================");
+                     String bayarPasien = input.nextLine();
+                    System.out.println("========================================");
+                    System.out.println("|       Pasien Nomor " + biodataPasien[iGlobal][7]);
+                        System.out.println("|=> Nama Pasien     : " + biodataPasien[iGlobal][0]);
+                        System.out.println("|=> Alamat Pasien   : " + biodataPasien[iGlobal][1]);
+                        System.out.println("|=> Nomer HP        : " + biodataPasien[iGlobal][2]);
+                        System.out.println("|=> Penyakit Pasien : " + biodataPasien[iGlobal][3]);
+                        System.out.println("|=> Kode Pasien     : " + biodataPasien[iGlobal][7]);
+                        System.out.println("========================================\n");
+                    
                     break;
 
                 case 2:
